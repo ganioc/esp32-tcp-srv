f5f4588a590930673e762b46410e0dd22f881ee4
yango3 <o3.yang@hotmail.com>
2020-05-21 16:24:02 +0800

    Add uart/

---
+++ components/uart/include/uart.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+void init_uart();
---
+++ components/uart/uart.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "include/uart.h"
+
+void init_uart()
+{
+  printf("\nint_uart()\n");
+}
