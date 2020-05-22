1fe640f29e8757d6d80a22d2c11d12571472bc1a
yango3 <o3.yang@hotmail.com>
2020-05-22 10:11:36 +0800

    Add queue.c

--- components/queue/include/queue.h
+++ components/queue/include/queue.h
@@ -1,3 +1,28 @@
 #pragma once
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "esp_log.h"
+#include "freertos/semphr.h"
+#include "freertos/task.h"
+
+#define QUEUE_LENGTH 3
+#define MSG_LEN 32
+#define MAX_MSG_NUM 4
+
+typedef struct QueueManager
+{
+  int flag; // 1 valid, 0 invalid
+  int sock_num;
+  QueueHandle_t rx_queue; // from tcp to uart_task
+  QueueHandle_t tx_queue;
+
+} QueueManager_t;
+
+typedef struct Msg
+{
+  char buf[MSG_LEN];
+} Msg_t;
+
 void init_queue();
--- components/queue/queue.c
+++ components/queue/queue.c
@@ -1,7 +1,52 @@
 #include <stdio.h>
 #include "./include/queue.h"
 
+static QueueManager_t xManager[QUEUE_LENGTH];
+static SemaphoreHandle_t xSemaphore = NULL;
+StaticSemaphore_t xSemaphoreBuffer;
+
 void init_queue()
 {
-  
+  xSemaphore = xSemaphoreCreateBinaryStatic(&xSemaphoreBuffer);
+}
+
+int add_queue(int sock)
+{
+  int i = 0;
+  if (xSemaphoreTake(xSemaphore, portMAX_DELAY) == pdTRUE)
+  {
+    for (i = 0; i < QUEUE_LENGTH; i++)
+    {
+      if (xManager[i].flag == 0)
+      {
+        xManager[i].sock_num = sock;
+        xManager[i].rx_queue = xQueueCreate(MAX_MSG_NUM, sizeof(Msg_t));
+        xManager[i].tx_queue = xQueueCreate(MAX_MSG_NUM, sizeof(Msg_t));
+        xManager[i].flag = 1;
+      }
+    }
+    xSemaphoreGive(xSemaphore);
+    return 0;
+  }
+  return -1;
+}
+
+int remove_queue(int sock)
+{
+  if (xSemaphoreTake(xSemaphore, portMAX_DELAY) == pdTRUE)
+  {
+    for (int i = 0; i < QUEUE_LENGTH; i++)
+    {
+      if (xManager[i].flag == 1 && xManager[i].sock_num == sock)
+      {
+        xManager[i].sock_num = 0;
+        vQueueDelete(xManager[i].rx_queue);
+        vQueueDelete(xManager[i].tx_queue);
+        xManager[i].flag = 0;
+      }
+    }
+    xSemaphoreGive(xSemaphore);
+    return 0;
+  }
+  return -1;
 }
