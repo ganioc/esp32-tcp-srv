b366b6a7c55c6154dfb1fd40e02cc33ef4dc34f1
yango3 <o3.yang@hotmail.com>
2020-05-27 09:57:27 +0800

    Set static IP

--- components/wifi/wifi.c
+++ components/wifi/wifi.c
@@ -72,24 +72,21 @@ static void event_handler(void *arg, esp_event_base_t event_base,
 }
 void init_sta()
 {
-  s_wifi_event_group = xEventGroupCreate();
 
+  s_wifi_event_group = xEventGroupCreate();
   tcpip_adapter_init();
-  // ESP_ERROR_CHECK(esp_netif_init());
+  // to use static IP
+  tcpip_adapter_ip_info_t static_IP_info;
+  IP4_ADDR(&static_IP_info.ip, 192, 168, 32, 108);
+  IP4_ADDR(&static_IP_info.gw, 192, 168, 32, 1);
+  IP4_ADDR(&static_IP_info.netmask, 255, 255, 255, 0);
+  tcpip_adapter_set_ip_info(TCPIP_ADAPTER_IF_STA, &static_IP_info);
 
   ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-  // esp_netif_t *my_sta = esp_netif_create_default_wifi_sta();
-
-  // esp_netif_dhcpc_stop(my_sta);
-
-  // esp_netif_ip_info_t ip_info;
-
-  // IP4_ADDR(&ip_info.ip, 192, 168, 32, 108);
-  // IP4_ADDR(&ip_info.gw, 192, 168, 32, 1);
-  // IP4_ADDR(&ip_info.netmask, 255, 255, 255, 0);
-
-  // esp_netif_set_ip_info(my_sta, &ip_info);
+  // stop dhcp
+  tcpip_adapter_dhcpc_stop(TCPIP_ADAPTER_IF_STA);
+  tcpip_adapter_set_ip_info(TCPIP_ADAPTER_IF_STA, &static_IP_info);
 
   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
   ESP_ERROR_CHECK(esp_wifi_init(&cfg));
